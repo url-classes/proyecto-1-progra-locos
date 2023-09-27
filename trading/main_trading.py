3ed50d7b2dde43e8bf1ea617b1fbf64690c98fe2
CCVela1017 <124698727+CCVela1017@users.noreply.github.com>
2023-09-26 23:10:03 -0600

    Creacion de main_comercio
    Creacion del menu inicial del modulo comercio

---
+++ trading/main_trading.py
@@ -0,0 +1,14 @@
+from main_crops import main
+
+def main_trading():
+    print('COMPRA Y VENTA')
+    print('Bienvenido a la sección de compra y venta de productos.')
+    print('OPCIONES:\n  ' +
+          '1 - Productos de cultivo\n  ' +
+          '2 - Productos de la granja de animales\n  ' +)
+    sel = input('Ingrese la sección a la que deseas ingresar: ')
+    if sel == '1':
+        pass
+    elif sel == '2':
+        pass # animales
+main_trading()
